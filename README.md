c34a772c648e1d8187fb1fab6ba25a8f611f0132
MorcilloSanz <amorcillosanz@gmail.com>
2022-04-22 16:21:05 +0200

    Anisotropic Diffusion and Gradient Descent (opposite)

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # AnisotropicDiffusion2DPointCloud
 Approximate points of a point cloud in a smart way
+
+## Anisotropic Diffusion
+Let  
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/omega.png)  
+
+denote a subset of the plane and  
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/function.png)  
+
+be the diffusion function. Then anisotropic diffusion is defined as:
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/definition.png)
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/c.png)
+
+Where **Δ** denotes the Laplacian, **∇** denotes the gradient, **∇ •** is the divergence and **c(x,y,t)** is the diffusion coefficient which controls the rate of diffusion
+
+## Approximation using Gradient Descent
+
+As gradient is a vector defined as:
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/gradient.png)
+
+Let *v(x,y)* be a point of the point cloud
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/gradientDescent.png)
+
+The gradient vector is always pointing at a maximum. So moving *v* in that direction should aproximate *v* to a maximum, moving in the opposite direction aproximates to a minimum.
+
+## Algorithm
+* Each point radiates temperature. All the points radiates the same temperature (1).
+* Calculate a 2D Grid. The temperature of each square is nothing but the sum of the temperatures radiated by the points inside of it (As the temperature of a point is 1 by default, the temperature of the square is the amount of points in the square).
+* Apply Anisotropic Diffusion equation to the Grid in order to smooth.
+* Apply Gradient Descent (opposite) in order to approximate the points to those regions which have more amount of points.
+> Keep in mind that we want to approximate the point to regions that contain more amount of points, which are maximums. So instead of moving in the opposite gradient direction, we'll move in the gradient direction. (The opposite of Gradient Descent)
+
+## Demo
+
+Random generated point cloud
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/pointcloud.png)
+
+Grid and temperatures
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/temperatureGrid.png)
+
+Temperature Anisotropic Diffusion
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/diffusionGrid.png)
+
+Approximated points (smooth)
+
+![alt text](https://github.com/MorcilloSanz/AnisotropicDiffusion2DPointCloud/blob/main/img/approximated.png)
---
+++ src/sketch.js
@@ -0,0 +1,232 @@
+/*
+Smooth a point cloud approximating the points in a smart way.
+Anisotropic Diffusion for smoothing 
+Gradient descent (opposite) for approximating
+
+In order to remove noise, remove the points that are isolated.
+
+https://en.wikipedia.org/wiki/Anisotropic_diffusion
+https://en.wikipedia.org/wiki/Gradient_descent
+
+Usage:
+  1- First, press A for smooth grid
+  2- Then, press X for approximating points
+
+Dev: MorcilloSanz
+Email: amorcillosanz@gmail.com
+GitHub: https://github.com/MorcilloSanz
+*/
+
+// Point cloud
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function generatePointCloud(n) {
+  points = [];
+  for(let i = 0; i < n; i ++) {
+    let x = Math.random() * width;
+    let y = Math.random() * height;
+    points.push(new Point(x, y));
+  }
+  return points;
+}
+
+function drawPointCloud(pointCloud) {
+  fill(255, 255, 150);
+  stroke(255, 255, 150);
+  for(let i = 0; i < pointCloud.length; i ++) {
+    let currentPoint = pointCloud[i];
+    ellipse(currentPoint.x - 1, currentPoint.y - 1, 2, 2);
+  }
+}
+
+// Diffusion
+function Square(x, y, squareSize, pointIndices) {
+  this.x = x;
+  this.y = y;
+  this.squareSize = squareSize;
+  this.temperature = pointIndices.length;
+  this.pointIndices = pointIndices;
+  
+  this.draw = function() {
+    const lambda = 1;
+    fill(lambda * 255, lambda * (255 - 255 / (this.temperature + 1)), 0);
+    rect(this.x, this.y, this.squareSize, this.squareSize);
+  }
+}
+
+function Grid(squareSize, pointCloud) {
+  this.squareSize = squareSize;
+  this.pointCloud = pointCloud;
+  this.squares = [];
+  
+  for(let j = 0; j < height / this.squareSize; j ++) {
+    for(let i = 0; i < width / this.squareSize; i ++) {
+      let x = 0 + (i * this.squareSize);
+      let y = 0 + (j * this.squareSize);
+      let pointIndices = [];
+      
+      for(let k = 0; k < pointCloud.length; k ++) {
+        let currentPoint = pointCloud[k];
+        if(currentPoint.x >= x && currentPoint.x <= x + this.squareSize &&
+          currentPoint.y >= y && currentPoint.y <= y + this.squareSize) {
+          pointIndices.push(k);
+        }
+      }
+      
+      let square = new Square(x, y, this.squareSize, pointIndices);
+      this.squares.push(square);
+    }
+  }
+  
+  this.draw = function() {
+    stroke(0);
+    for(let i = 0; i < this.squares.length; i ++)
+      this.squares[i].draw();
+  }
+}
+
+function anisotropicDiffusion(grid, lambda, k, iterations) {
+  if(iterations <= 0)
+    return;
+
+  let w = width / grid.squareSize;
+  let h = height / grid.squareSize;
+  
+  for(let i = 0; i < w; i ++) {
+    for(let j = 0; j < h; j ++) {
+      let currentSquare = grid.squares[i + j * w];
+      
+      let NI = 0, SI = 0, EI = 0, WI = 0;
+      let ignoreN = false, ignoreS = false, ignoreE = false, ignoreW = false;
+      
+      if(i == 0)            ignoreW = true;
+      else if(i >= w - 1)   ignoreE = true;
+      if(j == 0)            ignoreN = true;
+      else if(j >= h - 1)   ignoreS = true;
+
+      if(!ignoreN) NI = grid.squares[i + (j - 1) * w].temperature - currentSquare.temperature;
+      if(!ignoreS) SI = grid.squares[i + (j + 1) * w].temperature - currentSquare.temperature;
+      if(!ignoreE) EI = grid.squares[(i + 1) + j * w].temperature - currentSquare.temperature;
+      if(!ignoreW) WI = grid.squares[(i - 1) + j * w].temperature - currentSquare.temperature;
+      
+      let cN = exp( -pow(abs(NI) / k, 2) );
+      let cS = exp( -pow(abs(SI) / k, 2) );
+      let cE = exp( -pow(abs(EI) / k, 2) );
+      let cW = exp( -pow(abs(WI) / k, 2) );
+      
+      let temperature = currentSquare.temperature + lambda * (cN * NI + cS * SI + cE * EI + cW * WI);
+      currentSquare.temperature = temperature;
+    }
+  }
+  
+  anisotropicDiffusion(grid, lambda, k, iterations - 1);
+}
+
+function approximatePoints(grid, stepSize, iterations) {
+  if(iterations <= 0)
+    return;
+
+  let w = width / grid.squareSize;
+  let h = height / grid.squareSize;
+
+  for(let i = 0; i < w; i ++) {
+    for(let j = 0; j < h; j ++) {
+      let currentSquare = grid.squares[i + j * w];
+
+      // Finite differences gradient
+      let E = 0, W = 0, S = 0, N = 0;
+      let ignoreN = false, ignoreS = false, ignoreE = false, ignoreW = false;
+
+      if(i == 0)            ignoreW = true;
+      else if(i >= w - 1)   ignoreE = true;
+      if(j == 0)            ignoreN = true;
+      else if(j >= h - 1)   ignoreS = true;
+
+      let dx = 1, dy = 1;
+      if(!ignoreN) N = grid.squares[i + (j - dy) * w].temperature;
+      if(!ignoreS) S = grid.squares[i + (j + dy) * w].temperature;
+      if(!ignoreE) E = grid.squares[(i + dx) + j * w].temperature;
+      if(!ignoreW) W = grid.squares[(i - dx) + j * w].temperature;
+
+      let gradient = [
+        (E - W) / (2 * dx), 
+        (S - N) / (2 * dy)
+      ];
+
+      // Opposite gradient descent
+      for(let k = 0; k < currentSquare.pointIndices.length; k ++) {
+
+        let currentPoint = grid.pointCloud[currentSquare.pointIndices[k]];
+        currentPoint.x += stepSize * gradient[0];
+        currentPoint.y += stepSize * gradient[1];
+
+        // If the point is in a new square, update grid
+        for(let l = 0; l < grid.squares; l ++) {
+          let nextSquare = grid.squares[l];
+
+          if(currentPoint.x >= nextSquare.x && currentPoint.x <= nextSquare.x + grid.squareSize &&
+            currentPoint.y >= nextSquare.y && currentPoint.y <= nextSquare.y + grid.squareSize && 
+            nextSquare != currentSquare) {
+
+            // Move indices from squares
+            nextSquare.pointIndices.push(currentSquare.pointIndices[k]);
+            currentSquare.pointIndices.splice(k, 1);
+          }
+        }
+
+      }
+    }
+  }
+  approximatePoints(grid, stepSize, iterations - 1);
+}
+
+// Program
+const w = 760;
+const h = 760;
+
+const maxPoints = 1200;
+let pointCloud = [];
+let drawPoints = true;
+
+const squareSize = 20;
+let grid = null;
+let drawGrid = true;
+
+const lambda = 0.01;
+const k = 6;  // More variation when K is in [1, 6]
+let iterations = 40;
+
+const stepSize = 1;
+const approximationIterations = 5;
+
+function setup() {
+  createCanvas(w, h);
+  pointCloud = generatePointCloud(maxPoints);
+  grid = new Grid(squareSize, pointCloud);
+}
+
+function draw() {
+  background(100);
+  if(grid != null && drawGrid) grid.draw();
+  if(drawPoints) drawPointCloud(pointCloud);
+  
+  stroke(0);
+  fill(255);
+  text("A apply diffusion\nG toggle grid\nP toggle points\nX approximate points", 10, 20);
+}
+
+function keyPressed() {
+  if(key == "a") {
+    console.log("Applying diffusion");
+    anisotropicDiffusion(grid, lambda, k, iterations);
+  }     
+  else if(key == "g") drawGrid = !drawGrid;
+  else if(key == "p") drawPoints = !drawPoints;
+  else if(key == "x") {
+    console.log("Approximating");
+    approximatePoints(grid, stepSize, approximationIterations);
+  }
+}
